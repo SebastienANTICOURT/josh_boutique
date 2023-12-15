5904d07cb5a0c35414dd2d590378bab3496e7959
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-12-15 16:58:33 +0100

    classNames

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <p>hello world</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        Your marketplace for high-quality{" "}
+        <span className="text-blue-600">digital assets</span>.
+      </h1>
+      <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+        Welcome to DigitalHipp. Every asset on our platform is verifiex by our
+        team to ensure our highest quality standards
+      </p>
+    </MaxWidthWrapper>
+  );
 }
