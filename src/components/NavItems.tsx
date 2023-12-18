e9dc52e06290a4817d02961770f1c7b5b8155b0a
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-12-18 17:09:45 +0100

    navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,10 +1,23 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
 
 function NavItems() {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
 
   const isAnyOpen = activeIndex !== null;
 
